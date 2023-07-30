a64a993bfe46909bc760ed48f4f1406244eb82ce
Thamoddya Rashmitha <113902572+Thamoddya@users.noreply.github.com>
2023-07-30 20:47:45 +0530

    first commmit

---
+++ resources/views/components/portfolio/coursel.blade.php
@@ -0,0 +1,61 @@
+
+<div class="intro-content-2">
+    <div class="slider-images">
+        <img src="{{ asset('img/slider/h3.jpg') }}" alt="">
+    </div>
+    <div class="slider-content">
+        <div class="display-table">
+            <div class="display-table-cell">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <!-- layer 1 -->
+                            <div class="layer-1 wow fadeInUp" data-wow-delay="0.3s">
+                                <h2 class="title2"> Transform Your Business with Our Software Solutions.</h2>
+                            </div>
+                            <!-- layer 2 -->
+                            <div class="layer-2 wow fadeInUp" data-wow-delay="0.5s">
+                                <p>"Boost productivity and drive growth with our advanced software solutions tailored for your needs.</p>
+                            </div>
+                            <!-- layer 3 -->
+                            <div class="layer-3 wow fadeInUp" data-wow-delay="0.7s">
+                                <a href="#" class="ready-btn left-btn ">Our Services</a>
+                                <a href="#" class="ready-btn right-btn">Contact us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="intro-content-2 slide-2">
+    <div class="slider-images">
+        <img src="{{ asset('img/slider/h4.jpg') }}" alt="">
+    </div>
+    <div class="slider-content">
+        <div class="display-table">
+            <div class="display-table-cell">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <!-- layer 1 -->
+                            <div class="layer-1 wow fadeInUp" data-wow-delay="0.3s">
+                                <h2 class="title2"> Elevate Your Business with Our Software Solutions.</h2>
+                            </div>
+                            <!-- layer 2 -->
+                            <div class="layer-2 wow fadeInUp" data-wow-delay="0.5s">
+                                <p>Effortlessly streamline your operations with our top-notch IT solutions for unmatched efficiency.</p>
+                            </div>
+                            <!-- layer 3 -->
+                            <div class="layer-3 wow fadeInUp" data-wow-delay="0.7s">
+                                <a href="#" class="ready-btn left-btn ">Our Services</a>
+                                <a href="#" class="ready-btn right-btn">Contact us</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
